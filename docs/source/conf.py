a2e0034fa5b0b5257be459c492257fc92567f053
eldesh <nephits@gmail.com>
2022-04-18 17:35:55 +0900

    Bump version to 0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Takayuki Goto'
 author = 'Takayuki Goto'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
