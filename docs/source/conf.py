987b319cfe294cd53146bb582088529c125fd275
eldesh <nephits@gmail.com>
2022-04-18 17:19:10 +0900

    Fix: bump version to 0.0.2 in the conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Takayuki Goto'
 author = 'Takayuki Goto'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
